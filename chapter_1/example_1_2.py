882d36c015a8ebb118954517d9a06de7714fdd60
satyahbp <satya.hbp@gmail.com>
2026-03-29 21:20:52 +0530

    completed chapter 1

---
+++ chapter_1/example_1_2.py
@@ -0,0 +1,43 @@
+# vector implementation.
+
+import math
+
+class Vector:
+
+    def __init__(self, x=0, y=0):
+        self.x = x
+        self.y = y
+
+    
+    def __repr__(self):
+        return f"Vector({self.x!r}, {self.y!r})"
+    
+
+    def __abs__(self):
+        return math.hypot(self.x, self.y)
+    
+
+    def __bool__(self):
+        return bool(abs(self))
+    
+
+    def __add__(self, other):
+        x = self.x + other.x
+        y = self.y + other.y
+        return Vector(x, y)
+
+    
+    def __mul__(self, scalar):
+        return Vector(self.x * scalar, self.y * scalar)
+    
+
+v1 = Vector(2, 4)
+v2 = Vector(2, 1)
+
+print("repr: ", repr(v1))
+print(v1)
+
+print("add: ", v1 + v2)
+
+print("multiply: ", v1 * 3)
+print("bool:", bool(v1))
